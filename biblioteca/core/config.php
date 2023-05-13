31dde7ef59e0d2f56ef8b33ee5e28703ff79f4bd
ChessCream <107474465+ChessCream@users.noreply.github.com>
2023-05-13 14:03:05 -0600

    Delete biblioteca directory

--- biblioteca/core/config.php
+++
@@ -1,3 +0,0 @@
-<?PHP
-define("URL","http://localhost/biblioteca/");
-date_default_timezone_set("America/El_Salvador");
--- biblioteca/core/configApp.php
+++
@@ -1,8 +0,0 @@
-<?php
-//datos de configuracion de mysql
-const SERVER="localhost";
-const DB="biblioteca";
-const USER="root";
-const PASS="";
-
-const SGBD="mysql:host=".SERVER.";dbname=".DB;
